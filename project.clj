5ce26646c70662b051c3639abf5d38d3b2248fb1
Dan Neumann <danrodneu@gmail.com>
2013-09-23 16:35:32 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject forum "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.5"]]
+  :ring {:handler forum.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
