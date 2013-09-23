09921846ef12aa7a241e58de48a10f44e40bd148
Dan Neumann <danrodneu@gmail.com>
2013-09-23 16:38:33 -0500

    Add Datomic

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [com.datomic/datomic "0.8.3335"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler forum.handler/app}
   :profiles
